00cc1e3046fef3e21d879196bd501ba1a2f40e09
Incredeble <vaibhav.18bcs1050@abes.ac.in>
2021-05-12 23:36:28 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,request,jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app=app)
+
+@app.route('/api',methods=['GET'])
+
+def hello_world():
+    d={}
+    d['Query'] = str(request.args['Query'])
+    return jsonify(d)
+
+
+if __name__ == '__main__':
+    app.run()
+    
